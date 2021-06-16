160cca93eef51459b06c146470e6bec9e338d1b4
mr-linus <mrfunky001@gmail.com>
2021-06-17 00:40:10 +0800

    feat: add idt in init

--- init/entry.c
+++ init/entry.c
@@ -1,15 +1,22 @@
 #include "console.h"
 #include "debug.h"
+
 int kern_entry()
 {
 	init_debug();
-	init_gdt();
 
+	init_gdt();
+	init_idt();
 	console_clear();
 
 	printk_color(rc_black, rc_green, "Hello, CherryOS kernel!\n");
 
+	asm volatile ("int $0x3");
+    asm volatile ("int $0x4");
+
 	print_cur_status();
+
+	
 
 	panic("debug-panic");
 
